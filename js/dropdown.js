fb734f6a956b00eb9447736bdead0ae1d17ab4ad
jsr029 <jsr059@gmail.com>
2022-01-08 23:41:06 +0100

    Update

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,5 +1,5 @@
 function dropdown() {
-    let dropI = document.querySelector('.dropdown_ingredients');
+    let ingredientsResult = document.querySelector('.ingredientsResult');
     let buttonI = document.querySelector('.dropdown_ingredients-button');
     let iconI = document.querySelector('.dropdown_ingredients-button i');
     let h2I = document.querySelector('.dropdown_ingredients-button h2');
@@ -10,11 +10,33 @@ function dropdown() {
             h2I.style.display = 'none';
             inputI.style.display = 'block';
             buttonI.style.width = '600px';
+            ingredientsResult.style.display = 'flex';
         }else{
             iconI.className = 'icon-angle-down';
             h2I.style.display = 'block';
             inputI.style.display = 'none';
-            buttonI.style.width = '170px';
+            buttonI.style.width = '150px';
+            ingredientsResult.style.display = 'none';
+        }
+    });
+    let applianceResult = document.querySelector('.applianceResult');
+    let buttonA = document.querySelector('.dropdown_appliance-button');
+    let iconA = document.querySelector('.dropdown_appliance-button i');
+    let h2A = document.querySelector('.dropdown_appliance-button h2');
+    let inputA = document.querySelector('.dropdown_appliance-buttonSearch');
+    iconA.addEventListener('click', function (event) {
+        if (iconA.className == 'icon-angle-down') {
+            iconA.className = 'icon-angle-up';
+            h2A.style.display = 'none';
+            inputA.style.display = 'block';
+            buttonA.style.width = '290px';
+            applianceResult.style.display = 'flex';
+        }else{
+            iconA.className = 'icon-angle-down';
+            h2A.style.display = 'block';
+            inputA.style.display = 'none';
+            buttonA.style.width = '150px';
+            applianceResult.style.display = 'none';
         }
     });
 }
--- js/searchInput.js
+++ js/searchInput.js
@@ -14,40 +14,70 @@ function searchInput(data) {
     let ingre = ingred.uniqueIngredients;
     search.addEventListener('input', function (evt) {
         let globalInput = search.value;
-            if(globalInput.length > 3)
+        if (globalInput.length > 3)
             nameResult = [];
-            descriptionResult = [];
-            applianceResult = [];
-            ingredientsResult = [];
-            ustensilsResult = [];
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].name.toLowerCase().includes(globalInput.toLowerCase())) {
-                    nameResult.push(data[i]);
-                }
-                if (data[i].description.toLowerCase().includes(globalInput.toLowerCase())) {
-                    descriptionResult.push(data[i]);
-                }
-                if (data[i].appliance.toLowerCase().includes(globalInput.toLowerCase())) {
-                    applianceResult.push(data[i]);
-                }
-                for (let k = 0; k < data[i].ingredients.length; k++) {
-                    if (data[i].ingredients[k].ingredient.toLowerCase().includes(globalInput.toLowerCase())) {
-                        ingredientsResult.push(data[i]);
-                    }
-                }
-                for (let k = 0; k < data[i].ustensils.length; k++) {
-                    if (data[i].ustensils[k].toLowerCase().includes(globalInput.toLowerCase())) {
-                        ustensilsResult.push(data[i]);
-                    }
+        descriptionResult = [];
+        applianceResult = [];
+        ingredientsResult = [];
+        ustensilsResult = [];
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].name.toLowerCase().includes(globalInput.toLowerCase())) {
+                nameResult.push(data[i]);
+            }
+            if (data[i].description.toLowerCase().includes(globalInput.toLowerCase())) {
+                descriptionResult.push(data[i]);
+            }
+            if (data[i].appliance.toLowerCase().includes(globalInput.toLowerCase())) {
+                applianceResult.push(data[i]);
+            }
+            for (let k = 0; k < data[i].ingredients.length; k++) {
+                if (data[i].ingredients[k].ingredient.toLowerCase().includes(globalInput.toLowerCase())) {
+                    ingredientsResult.push(data[i]);
                 }
             }
+            for (let k = 0; k < data[i].ustensils.length; k++) {
+                if (data[i].ustensils[k].toLowerCase().includes(globalInput.toLowerCase())) {
+                    ustensilsResult.push(data[i]);
+                }
+            }
+        }
         newBaseFilteredConcated = [...new Set([...nameResult, ...ingredientsResult, ...descriptionResult, ...applianceResult, ...ustensilsResult])];
         newBaseFilteredConcated.sort((a, b) => b.name.localeCompare(a.name));
         new DisplayRecipes().render(newBaseFilteredConcated);
     });
-    console.log(newBaseFilteredConcated);
     //Affiche les recettes par ordre alphabetique
     recepiesSort = data.sort((a, b) => b.name.localeCompare(a.name));
     new DisplayRecipes().render(recepiesSort);
+    dropIList(data);
 }
+let dropIList = function (data) {
+    let dropList = document.querySelector('.ingredientsResult');
+    let boxIList = [];
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].ingredients.length; j++) {
+            boxIList.push(`
+        <a class="dropdown-item" href="javascript:void(0)">
+            ${data[i].ingredients[j].ingredient}
+        </a>
+        `);
+        }
+    }
+    boxIList = [...new Set(boxIList)];
+    boxIList.sort();
+    dropList.innerHTML = boxIList;
+
+    let dropListA = document.querySelector('.applianceResult');
+    let boxIListA = [];
+    for (let j = 0; j < data.length; j++) {
+            boxIListA.push(`
+        <a class="dropdown-item" href="javascript:void(0)">
+            ${data[j].appliance}
+        </a>
+        `);
+    }
+    boxIListA = [...new Set(boxIListA)];
+    boxIListA.sort();
+    dropListA.innerHTML = boxIListA;
+
+};
 export default searchInput;
